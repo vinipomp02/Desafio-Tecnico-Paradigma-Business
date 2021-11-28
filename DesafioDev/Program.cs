61ac95ba4ba785e994761cf9cced767e74011a18
Vinicius Pompolino Meniquelli da Silva <vinipomp@gmail.com>
2021-11-28 17:38:51 -0300

    Adicionando novos arquivos

---
+++ DesafioDev/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesafioDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listaFinal = new int[7] { 7, 5, 3, 9, 6, 4, 1 };
+            int[] listaInicial = new int[7];
+            listaFinal.CopyTo(listaInicial, 0);
+
+            for (int i = 0; i < listaFinal.Length; i++)
+            {
+                listaFinal[i] = (listaFinal[i] == 9) ? 5 : listaFinal[i];
+            }
+
+            listaFinal = listaFinal.Where((source, index) => index != 5).ToArray();
+
+            Console.WriteLine($"Array Inicial: [{string.Join(" , ", listaInicial)}]");
+            Console.WriteLine($"Array Final: [{string.Join(" , ", listaFinal)}]");
+            Console.WriteLine($"A Soma dos Valores do Array Final é: {listaFinal.Sum(x => x)}");
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ DesafioDev3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesafioDev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] lista = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            bool contemConta = false;
+
+            Console.Write("Digite o valor da soma esperada: ");
+            int valorParseado = 0;
+            if (!int.TryParse(Console.ReadLine(), out valorParseado))
+            {
+                Console.WriteLine("Valor digitado inválido. Digite apenas inteiros");
+                return;
+            }
+            for (int i = 0; i < lista.Length - 1; i++)
+            {
+                for (int j = i + 1; j < lista.Length; j++)
+                {
+                    if (lista[i] + lista[j] == valorParseado)
+                    {
+                        Console.WriteLine($"{valorParseado} é a soma de {lista[i]} + {lista[j]}");
+                        contemConta = true;
+                    }
+                    if (contemConta) return;
+                }
+            }
+            if (!contemConta) Console.WriteLine("Nenhuma Soma Possível dentre os valores do Array");
+        }
+    }
+}
